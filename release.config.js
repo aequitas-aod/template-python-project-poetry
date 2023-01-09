648759ba41fda0cad343493709a57bcb908f7229
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2023-01-09 13:51:42 +0100

    ci: enable semantic release

---
+++ release.config.js
@@ -0,0 +1,19 @@
+var publishCmd = `
+python -m twine upload dist/*
+`
+var config = require('semantic-release-preconfigured-conventional-commits');
+config.plugins.push(
+    [
+        "@semantic-release/exec",
+        {
+            "publishCmd": publishCmd,
+        }
+    ],
+    ["@semantic-release/github", {
+        "assets": [ 
+            { "path": "dist/*" },
+         ]
+    }],
+    "@semantic-release/git",
+)
+module.exports = config
