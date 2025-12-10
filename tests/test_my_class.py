c81c5dbd0705701fe732fefaed87fac13f9a32bb
Mattia Matteini <matteinimattia@gmail.com>
2025-12-10 11:58:29 +0100

    feat: add ruff for formatting and static checking

--- tests/test_my_class.py
+++ tests/test_my_class.py
@@ -5,7 +5,6 @@ from my_project import MyClass
 
 # can test using classes, name must begin with `Test`
 class TestMyClass:
-
     # test methods' names must begin with `test_`
     def test_my_method(self):
         x = MyClass().my_method()
@@ -16,14 +15,18 @@ class TestMyClass:
 def test_sum():
     assert 1 + 2 == pytest.approx(3)
 
+
+# pytest fixture example
 @pytest.fixture
 def sample_fixture():
     return [42]
+
 
 def test_sample_fixture(sample_fixture):
     assert sample_fixture == [42]
     sample_fixture.append(43)
     assert sample_fixture == [42, 43]
+
 
 def test_sample_fixture_isolated(sample_fixture):
     assert sample_fixture == [42]
