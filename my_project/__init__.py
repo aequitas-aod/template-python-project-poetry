7b515ccef3ee34739a8bd3bf1296a9380e23aa78
Mattia Matteini <matteinimattia@gmail.com>
2025-12-10 11:58:29 +0100

    feat: add ruff for formatting and static checking

--- my_project/__init__.py
+++ my_project/__init__.py
@@ -2,7 +2,8 @@ import logging
 
 
 logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger('my_project')
+logger = logging.getLogger("my_project")
+
 
 # this is the initial module of your app
 # this is executed whenever some client-code is calling `import my_project` or `from my_project import ...`
